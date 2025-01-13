eabea3d8e1f8d9832e44a0a23ddd59422e22f7c4
y1z <whyis2cool@gmail.com>
2025-01-13 01:40:29 -0600

    Start working on day3

---
+++ src/day3.rs
@@ -0,0 +1,8 @@
+use crate::utility;
+
+
+pub fn start() {
+    let data = utility::read_file("files/day_03.txt");
+    println!("{}", data);
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 pub(crate) mod day1;
 pub(crate) mod day2;
+pub(crate) mod day3;
+
 mod utility;
 
 fn main() {
-    day2::start();
+    day3::start();
 }
